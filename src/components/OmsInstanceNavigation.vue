e9a67460b187dd12ffe4bb369892eb878aba2d0e
amansinghbais <singh11amanbais@gmail.com>
2023-09-18 12:24:11 +0530

    Fixed: indentation (#140)

--- src/components/OmsInstanceNavigation.vue
+++ src/components/OmsInstanceNavigation.vue
@@ -8,11 +8,9 @@
 				{{ appUserState.instanceUrl }}
 			</ion-card-title> 
 		 </ion-card-header>
-
 		<ion-card-content> 
 			{{ 'This is the name of the OMS you are connected to right now. Make sure that you are connected to the right instance before proceeding.' }} -->
 		</ion-card-content> 
-
 		<ion-button @click="goToOms(appUserState.userToken, appUserState.instanceUrl)" fill="clear">
 			{{ 'Go to OMS' }}
 			<ion-icon slot="end" :icon="openOutline" />
@@ -21,17 +19,16 @@
 </template>
 
 <script setup lang="ts">
-	import {IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonLabel, IonIcon} from '@ionic/vue'
-	import {openOutline} from 'ionicons/icons'
-	import {appContext,goToOms} from '../index'
-	import {computed} from 'vue'
+import {IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonLabel, IonIcon} from '@ionic/vue'
+import {openOutline} from 'ionicons/icons'
+import {appContext,goToOms} from '../index'
+import {computed} from 'vue'
 
-	const store = appContext.config.globalProperties.$store;
-	const appUserState = computed(() => {
-		return {
-			instanceUrl: store.getters['user/getInstanceUrl'],
-			userToken: store.getters['user/getUserToken']
-		}
-	})
-
+const store = appContext.config.globalProperties.$store;
+const appUserState = computed(() => {
+  return {
+    instanceUrl: store.getters['user/getInstanceUrl'],
+    userToken: store.getters['user/getUserToken']
+  }
+})
 </script>
